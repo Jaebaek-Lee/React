c56d6f9e58a9c52ba499b498bde6ea654fa5b4f8
Lee Jae-Baek <103738589+ThinkingVincent@users.noreply.github.com>
2022-09-23 02:51:46 +0900

    React.js_form_create

--- App.js
+++ App.js
@@ -43,18 +43,39 @@ function Article(props) {
   );
 }
 
+function Create(props) {
+  return (
+    <article>
+      <h2>Create</h2>
+      <form onSubmit = {event => {event.preventDefault();
+        const title = event.target.title.value;
+        const body = event.target.body.value;
+        props.onCreate(title, body);}}>
+        <p><input type = "text" name = "title" placeholder="title"></input></p>
+        <p><textarea name = "body" placeholder = "body"></textarea></p>
+        <p><input type = "submit" value="Create"></input></p>
+      </form>
+    </article>
+  );
+}
+
 function App() {
   const [mode, setMode] = useState('WELCOME');
   const [id, setId] = useState(null);
-  const topics = [{id:1, title:'HTML', body:'HTML is ...'},
+  const [nextId, setNextId] = useState(4);
+  const [topics, setTopics] = useState([
+                  {id:1, title:'HTML', body:'HTML is ...'},
                   {id:2, title:'CSS', body:'CSS is ...'},
                   {id:3, title:'Javascript', body:'Javascript is ...'}
-                ];
+                ]);
+
   let content = null;
-  if (mode == 'WELCOME') {
+
+  if (mode === 'WELCOME') {
     content = <Article title = "Welcome" body = "Hello, JaeBaek."></Article>
   }
-  else if (mode == 'READ') {
+
+  else if (mode === 'READ') {
     let title, body = null;
     for(let i = 0; i < topics.length; ++i) {
       if (topics[i].id === id) {
@@ -65,11 +86,24 @@ function App() {
     content = <Article title = {title} body = {body}></Article>
   }                
 
+  else if (mode === 'CREATE') {
+    content = <Create onCreate = {(_title, _body) => {
+      const newTopic = {id:nextId, title:_title, body:_body};
+      const newTopics = [...topics];
+      newTopics.push(newTopic);
+      setTopics(newTopics);
+      setId(nextId);
+      setNextId(nextId + 1);
+      setMode('READ');
+    }}></Create>
+  }
+
   return (
     <div>
       <Header title = "WEB" onChangeMode = {() => {setMode('WELCOME');}}></Header>
       <Nav topics = {topics} onChangeMode = {(id) => {setMode('READ'); setId(id);}}></Nav>
       {/* setmode하면 다시 앞으로 돌아가나? ㅇㅇ 리턴 밖만.*/}
+      <a href = "/create" onClick = {event => {event.preventDefault(); setMode('CREATE');}}>Create</a>
       {content}
     </div>
   );
